44074c6a62953120c059efee7a6e3f59d816c968
Said-Nuri <datron1111@gmail.com>
2017-09-05 21:42:49 +0300

    örnek pluginler oluşturuldu

--- app/src/client/controllers/plugin.controller.js
+++ app/src/client/controllers/plugin.controller.js
@@ -16,39 +16,91 @@ app.controller('PluginController', ['$scope' ,function ($scope) {
             }
         },
         left2 : {
-            'margin_top': '5px',
-            'margin_bottom': '5px',
-            'header': 'Başlık 2'
+            margin_top: '5px',
+            margin_bottom: '5px',
+            header : 'Eğitimler',
+            list: {
+                appsec:{
+                    title: 'application security'
+                },
+                netsec: {
+                    title: 'network security'
+                },
+                endpsec: {
+                    title: 'endpoint security'
+                }
+            }
         },
         left3 : {
             'margin_top': '5px',
             'margin_bottom': '5px',
-            'header': 'Başlık 3'
+            'header': 'Merak Edilen Konular',
+            list: {
+                konu1: {
+                    title: 'Hacker kime denir/denmez?'
+                },
+                konu2: {
+                    title: 'Güvenlik açığı nedir? nasıl oluşur?'
+                },
+                konu3: {
+                    title: 'Büyük saldırılar nasıl yapılır?'
+                },
+                konu4: {
+                    title: 'Siber ordu nedir?'
+                }
+            }
         },
         left4: {
             'margin_top': '5px',
             'margin_bottom': '5px',
-            'header': 'Başlık 4'
+            'header': 'Güvenlik Önerileri',
+            list: {
+                konu1: {
+                    title: 'Antivirüs Önerileri'
+                },
+                konu2: {
+                    title: 'güvenli alışveriş'
+                },
+                konu3: {
+                    title: 'güçlü şifre nasıl oluşturulur.'
+                }
+            }
+        },
+        left5: {
+            'margin_top': '5px',
+            'margin_bottom': '5px',
+            'header': 'Saçma sapan sorular ve düşünceler',
+            list: {
+                konu1: {
+                    title: 'Falan kişinin facesini patlatabilir misin?'
+                },
+                konu2: {
+                    title: 'Antivirüse ihtiyacım yok.'
+                },
+                konu3: {
+                    title: 'Ben hekırım olm'
+                }
+            }
         },
         right1: {
             'margin_top': '0px',
             'margin_bottom': '5px',
-            'header': 'Başlık 1'
+            'header': 'Son paylaşımlar'
         },
         right2: {
             'margin_top': '5px',
             'margin_bottom': '5px',
-            'header': 'Başlık 2'
+            'header': 'Durum Bilginiz'
         },
         right3: {
             'margin_top': '5px',
             'margin_bottom': '5px',
-            'header': 'Başlık 3'
+            'header': 'Bizi takip edin'
         },
         right4: {
             'margin_top': '5px',
             'margin_bottom': '5px',
-            'header': 'Başlık 4'
+            'header': 'Abone ol, Siber olaylardan haberin olsun'
         }
     };
 }]);
