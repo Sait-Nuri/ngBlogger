ea2380f828f04c66e718057b40894f178c4c96e2
Said-Nuri <said000332>
2017-09-12 23:51:37 +0300

    Yorum kısmı bitmek üzere, drop-down tarzı olacak

---
+++ app/src/client/directives/write_comment.directive.js
@@ -0,0 +1,12 @@
+app.directive('writeComment', function() {
+
+    var directive = {};
+    directive.restrict = 'E';
+    directive.templateUrl = "partials/directive.write_comment.html";
+
+    directive.scope = {
+
+    };
+
+    return directive;
+});
---
+++ app/src/client/directives/write_reply.directive.js
@@ -0,0 +1,12 @@
+app.directive('writeReply', function() {
+
+    var directive = {};
+    directive.restrict = 'E';
+    directive.templateUrl = "partials/directive.write_reply.html";
+
+    directive.scope = {
+
+    };
+
+    return directive;
+});
