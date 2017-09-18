8473627e31895a5528f6cadffe8ed3f10d5e1095
Said-Nuri <datron1111@gmail.com>
2017-09-19 01:14:30 +0300

    Yorum yazma kısmı düzeltildi. Gönder ve Reply butonu eklenecek

--- app/src/client/directives/comment_base.directive.js
+++ app/src/client/directives/comment_base.directive.js
@@ -1,8 +1,8 @@
-app.directive('writeReply', function() {
+app.directive('commentBase', function() {
 
     var directive = {};
     directive.restrict = 'E';
-    directive.templateUrl = "partials/directive.write_reply.html";
+    directive.templateUrl = "partials/directive.comment_base.html";
 
     directive.scope = {
 
---
+++ app/src/client/directives/comment_reply.directive.js
@@ -0,0 +1,12 @@
+app.directive('commentReply', function() {
+
+    var directive = {};
+    directive.restrict = 'E';
+    directive.templateUrl = "partials/directive.comment_reply.html";
+
+    directive.scope = {
+
+    };
+
+    return directive;
+});
